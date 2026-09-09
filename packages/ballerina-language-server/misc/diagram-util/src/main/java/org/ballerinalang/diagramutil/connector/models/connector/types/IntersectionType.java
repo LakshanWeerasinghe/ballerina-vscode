d4a7c8e33ecf67c4d44ff697059537b5a6303694
Thuvarakan Sritharan <sthuva98@gmail.com>
2026-09-09 08:43:22 +0530

    Fix aliasing of cached intersection types in record config model

--- packages/ballerina-language-server/misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/connector/models/connector/types/IntersectionType.java
+++ packages/ballerina-language-server/misc/diagram-util/src/main/java/org/ballerinalang/diagramutil/connector/models/connector/types/IntersectionType.java
@@ -35,4 +35,16 @@ public class IntersectionType extends Type {
         this.typeName = "intersection";
         this.members = new ArrayList<>();
     }
+
+    public IntersectionType(IntersectionType intersectionType) {
+        this.typeName = intersectionType.typeName;
+        this.members = intersectionType.members;
+        this.name = intersectionType.name;
+        this.optional = intersectionType.optional;
+        this.typeInfo = intersectionType.typeInfo;
+        this.defaultable = intersectionType.defaultable;
+        this.defaultValue = intersectionType.defaultValue;
+        this.displayAnnotation = intersectionType.displayAnnotation;
+        this.documentation = intersectionType.documentation;
+    }
 }
